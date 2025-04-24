b9390aaeca0bb849e332d2863513350ee5a3833d
benja pinto <be.pintor@duocuc.cl>
2025-04-24 11:01:28 -0400

    inicial

---
+++ src/main/java/com/duoc/SpringApp_Grupo5/SpringAppGrupo5Application.java
@@ -0,0 +1,13 @@
+package com.duoc.SpringApp_Grupo5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAppGrupo5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAppGrupo5Application.class, args);
+	}
+
+}
