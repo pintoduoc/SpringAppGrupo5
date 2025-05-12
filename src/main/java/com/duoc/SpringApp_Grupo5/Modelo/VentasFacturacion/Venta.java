9b276a8cc5525c04204828b6170a88e2b89bc324
CETECOM <CETECOM@AVAAOV02LC0401.duoc.acad>
2025-05-12 09:54:25 -0400

    Terminacion de clase usuario
    Terminacion de clase usuario

--- src/main/java/com/duoc/SpringApp_Grupo5/Modelo/VentasFacturacion/Venta.java
+++ src/main/java/com/duoc/SpringApp_Grupo5/Modelo/VentasFacturacion/Venta.java
@@ -1,5 +1,7 @@
 package com.duoc.SpringApp_Grupo5.Modelo.VentasFacturacion;
 
+import com.duoc.SpringApp_Grupo5.Modelo.GestionInventario.Producto;
+import com.duoc.SpringApp_Grupo5.Modelo.Usuarios.Usuario;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -7,6 +9,9 @@ import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 @AllArgsConstructor
@@ -18,7 +23,8 @@ public class Venta {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int idVenta;
-    private Usuario
+    private Usuario usuario;
+    private List<Producto> productosVenta = new ArrayList<>();
 
 
 }
