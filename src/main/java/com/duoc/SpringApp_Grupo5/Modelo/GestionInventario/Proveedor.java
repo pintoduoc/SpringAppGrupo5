ffe0ed44e0acbb75b3128bdf9334e600b7dc6e83
Oscar <osca.sepulveda@duocuc.cl>
2025-05-12 09:53:19 -0400

    producto y proveedor :3

--- src/main/java/com/duoc/SpringApp_Grupo5/Modelo/GestionInventario/Proveedor.java
+++ src/main/java/com/duoc/SpringApp_Grupo5/Modelo/GestionInventario/Proveedor.java
@@ -1,51 +1,23 @@
 package com.duoc.SpringApp_Grupo5.Modelo.GestionInventario;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+
+@Entity
 public class Proveedor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String nombre, contacto, direccion;
-
-    //Constructores:
-    public Proveedor() {
-    }
-
-    public Proveedor(String nombre, int id, String contacto, String direccion) {
-        this.nombre = nombre;
-        this.id = id;
-        this.contacto = contacto;
-        this.direccion = direccion;
-    }
-
-
-    //Getter & Setter:
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getContacto() {
-        return contacto;
-    }
-
-    public void setContacto(String contacto) {
-        this.contacto = contacto;
-    }
-
-    public String getDireccion() {
-        return direccion;
-    }
-
-    public void setDireccion(String direccion) {
-        this.direccion = direccion;
-    }
 }
