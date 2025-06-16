b13f4baee789a347dc0a004796c73effe517d475
Agustin Morales <ag.moralesp@duocuc.cl>
2025-06-16 09:53:30 -0400

    Creacion de Tags para Usuario y Venta

--- src/main/java/com/duoc/SpringApp_Grupo5/Controller/Usuarios/UsuarioController.java
+++ src/main/java/com/duoc/SpringApp_Grupo5/Controller/Usuarios/UsuarioController.java
@@ -2,11 +2,14 @@ package com.duoc.SpringApp_Grupo5.Controller.Usuarios;
 
 import com.duoc.SpringApp_Grupo5.Modelo.Usuarios.Usuario;
 import com.duoc.SpringApp_Grupo5.Service.Usuarios.UsuarioService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/usuario")
+@Tag(name = "Usuarios", description = "Operaciones para la clase 'Usuario'")
 
 public class UsuarioController {
 
@@ -14,18 +17,23 @@ public class UsuarioController {
     private UsuarioService usuarioService;
 
     @GetMapping
+    @Operation(summary = "Obtener todos los usuarios", description = "Devuelve todos los usuarios registrados")
     public String getAllUsuarios() {return usuarioService.getAllUsuarios(); }
 
     @PostMapping
+    @Operation(summary = "Añadir Usuario", description = "Registra un usuario a la base de datos. Requiere: nombre, email, contraseña y rol ")
     public String addUsuario(@RequestBody Usuario usuario) {return usuarioService.addUsuario(usuario); }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Buscar usuario", description = "Busca un usuario mediante la id ingresada")
     public String getUsuarioById(@PathVariable int id) {return usuarioService.getUsuarioById(id); }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Borrar usuario", description = "Borra un usuario mediante la id ingresada")
     public String deleteUsuario(@PathVariable int id) {return usuarioService.deleteUsuario(id); }
 
     @PutMapping("/{id}")
+    @Operation(summary = "Actualizar usuario", description = "Actualiza un usuario. Requiere: id del usuario a actualizar, nuevo nombre, nuevo email, nueva contraseña, nuevo rol")
     public String updateUsuario(@PathVariable int id, @RequestBody Usuario usuario){
         return usuarioService.updateUsuario(id, usuario);
     }
--- src/main/java/com/duoc/SpringApp_Grupo5/Controller/VentasFacturacion/VentaController.java
+++ src/main/java/com/duoc/SpringApp_Grupo5/Controller/VentasFacturacion/VentaController.java
@@ -2,11 +2,14 @@ package com.duoc.SpringApp_Grupo5.Controller.VentasFacturacion;
 
 import com.duoc.SpringApp_Grupo5.Modelo.VentasFacturacion.Venta;
 import com.duoc.SpringApp_Grupo5.Service.VentasFacturacion.VentaService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/venta")
+@Tag(name = "Venta", description = "Operaciones para la clase 'Venta'")
 
 public class VentaController {
 
@@ -14,18 +17,23 @@ public class VentaController {
     private VentaService ventaService;
 
     @GetMapping
+    @Operation(summary = "Obtener todas las ventas", description = "Devuelve una lista con todas las ventas registradas")
     public String getAllVentas() {return ventaService.getAllVentas(); }
 
     @PostMapping
+    @Operation(summary = "Registrar venta", description = "Registra una venta a la base de datos. Requiere: Usuario y productos")
     public String addVenta(@RequestBody Venta venta) {return ventaService.addVenta(venta); }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Buscar venta", description = "Busca una venta mediante la id ingresada")
     public String getVentaById(@PathVariable int id) {return ventaService.getVentaById(id); }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Borrar venta", description = "Borra una venta mediante la id ingresada")
     public String deleteVenta(@PathVariable int id) {return ventaService.deleteVenta(id); }
 
     @PutMapping("/{id}")
+    @Operation(summary = "Actualizar venta", description = "Actualiza una venta. Requiere: id de la venta a actualizar, nuevo usuario y nuevo producto")
     public String updateVenta(@PathVariable int id, @RequestBody Venta venta){
         return ventaService.updateVenta(id, venta);
     }
